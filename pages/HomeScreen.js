f05502836217f98f7114ae64a5868d1fa8062a4f
brvlyd <bravelydirgayuska@gmail.com>
2024-09-27 12:08:12 +0700

    commit1

---
+++ pages/HomeScreen.js
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet } from "react-native";
+function HomeScreen() {
+return (
+<View style={styles.container}>
+<Text>HomeScreen</Text>
+</View>
+);
+}
+const styles = StyleSheet.create({
+    container: {
+     flex: 1,
+     alignItems: "center",
+     justifyContent: "center"
+}
+});
+export default HomeScreen;
