c3ac113e6ee13f7a9742b58abb99b4c61cffb2f1
brvlyd <bravelydirgayuska@gmail.com>
2024-09-27 12:14:30 +0700

    commit2

---
+++ data/data.js
@@ -0,0 +1,33 @@
+export const data = [
+    {
+    id: 1,
+    imageUrl:
+    "https://avatars.githubusercontent.com/u/116475964?v=4",
+    nama: "Bravely Dirgayuska",
+    nim: "21120122140142",
+    },
+    {
+    id: 2,
+    imageUrl:
+    "https://avatars.githubusercontent.com/u/26102002?v=4",
+    nama: "Dzikri Razzan Athallah",
+    nim: "21120122140163",
+    
+    },
+    {
+    id: 3,
+    imageUrl:
+    "https://avatars.githubusercontent.com/u/26102002?v=4",
+    nama: "Muhammad Zein Al-Kautsar Nugroho",
+    nim: "21120122140151",
+        
+    },
+    {
+    id: 4,
+    imageUrl:
+    "https://avatars.githubusercontent.com/u/26102002?v=4",
+    nama: "Muhammad Farhan Ramadhan",
+    nim: "21120122140088",
+            
+    },
+    ];
--- pages/ProfileScreen.js
+++ pages/ProfileScreen.js
@@ -1,68 +1,27 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, FlatList, StyleSheet } from "react-native";
 
-const Card = ({ dataNama }) => {
-  const imageUrl = dataNama.imageUrl
-    ? dataNama.imageUrl
-    : "https://avatars.githubusercontent.com/u/116475964?v=4";
-
-  return (
-    <View
-      style={{
-        paddingVertical: 8,
-        borderRadius: 8,
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <View
-        style={{
-          borderWidth: 2,
-          borderRadius: 8,
-          flexDirection: "row",
-          justifyContent: "flex-start",
-          alignItems: "center",
-          width: 300,
-        }}
-      >
-        <Image
-          source={{ uri: imageUrl }}
-          style={{
-            width: 100,
-            height: 100,
-            borderRightWidth: 2,
-            borderBottomLeftRadius: 6,
-            borderTopLeftRadius: 6,
-            borderColor: "black",
-          }}
-        />
-        <View
-          style={{
-            flexDirection: "column",
-            marginLeft: 16,
-            justifyContent: "center",
-            maxWidth: 180,
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 18,
-              fontWeight: "600",
-            }}
-          >
-            {dataNama.nama}
-          </Text>
-          <Text
-            style={{
-              fontWeight: "400",
-              color: "gray",
-            }}
-          >
-            {dataNama.nim}
-          </Text>
-        </View>
-      </View>
-    </View>
-  );
-};
-
-export default Card;
+import { data } from "../data/data";
+import Card from "../components/Card";
+import Header from "../components/Header";
+function ProfileScreen() {
+return (
+<View style={style.container}>
+<Header headerText={"Kelompok 01"} flexPosition={"center"}/>
+<FlatList
+showsVerticalScrollIndicator={false}
+legacyImplementation={false}
+data={data}
+renderItem={({ item }) => <Card dataNama={item} />}
+keyExtractor={(item) => item.id}
+/>
+</View>
+);
+}
+const style = StyleSheet.create({
+container: {
+flex: 1,
+backgroundColor: "white",
+padding: 16,
+},
+});
+export default ProfileScreen;
